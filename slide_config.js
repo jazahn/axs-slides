b0125f778a697ccb10e03705245f0245a8b9d3b8
jazahn <jcleveng@fas.harvard.edu>
2013-12-25 23:46:09 -0500

    adding config data, populating the second slide

--- slide_config.js
+++ slide_config.js
@@ -1,8 +1,8 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
+    title: 'Accessibility<br>in Education',
+    subtitle: 'with HTML5',
     //eventInfo: {
     //  title: 'Google I/O',
     //  date: '6/x/2013'
@@ -22,12 +22,12 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title<br>Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    name: 'JaZahn Clevenger',
+    company: 'Sr Software Engineer<br>Academnic Technology',
+    gplus: '',
+    twitter: '@jazahn',
+    www: 'http://blogs.law.harvard.edu/acts',
+    github: 'http://github.com/jazahn'
   }/*, {
     name: 'Second Name',
     company: 'Job Title, Google',
